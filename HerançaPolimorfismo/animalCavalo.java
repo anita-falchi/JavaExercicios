142264f1827cbb1364993b1bf5ed144f9a1f11d1
anita-falchi <93739689+anita-falchi@users.noreply.github.com>
2022-02-09 15:23:20 -0300

    Add files via upload

---
+++ HerançaPolimorfismo/animalCavalo.java
@@ -0,0 +1,10 @@
+package HerançaPolimorfismo;
+
+public class animalCavalo extends Animal {
+	public void emiteSom()
+	{
+		System.out.println(" iiirrrrí, rilinchin. Emite som de cavalo.");
+	}
+	
+
+}
---
+++ HerançaPolimorfismo/animalPreguica.java
@@ -0,0 +1,16 @@
+package HerançaPolimorfismo;
+
+public class animalPreguica extends Animal
+{
+	//metodo
+	public void emiteSom()
+	{
+		System.out.println("emite som de preguiça");
+	}
+	
+	public void subir()
+	{
+		System.out.println("o animal pode subir em árvores");
+	}
+	
+}
